f734a7a31c1e3cbce392aca15071021683395024
sjtommylee <sjtommylee@gmail.com>
2024-02-17 12:59:08 -0800

    lua snip

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- remap - this is mainly here to take care of the leader issue with lazynvim.
 require("core.remap")
-
 -- plugin manager
 require("plugins.lazy-nvim")
 
@@ -12,18 +11,27 @@ require("core.lspconfig")
 -- themes
 -- require("theme.tokyonight")
 require("theme.vscode")
+-- require("theme.rosepine")
+
+
+
 -- language severs
 require("language.tsserver")
-require("plugins.zenmode")
+
+-- mason 
 require("mason.mason")
 require("mason.mason-lspconfig")
+
+
+
+
+require("plugins.zenmode")
 require("plugins.autopairs")
 require("plugins.harpoon")
 require("plugins.telescope")
 require("plugins.tmux")
-
-
-
+require("plugins.cmp")
+require("plugins.luasnip")
 
 
 
